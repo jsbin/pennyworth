fc768fe7fbcab424bbadcc71a2d580524aa57cf5
Fabien O'Carroll <fabien@allou.is>
2014-06-18 16:31:07 +0100

    Add tests for jade

---
+++ test/jade/jade.test.js
@@ -0,0 +1,49 @@
+/* global describe, it, after, before */
+'use strict';
+
+var fs = require('fs');
+
+var should = require('should');
+
+var axon = require('axon');
+var requester = axon.socket('req');
+
+describe('Jade', function () {
+
+  before(function () {
+    requester.connect('tcp://localhost:5555');
+  });
+
+  it('Should process valid Jade and pass back the compiled source', function (done) {
+    fs.readFile(__dirname + '/sample.jade', function (error, file) {
+      requester.send({
+        language: 'jade',
+        source: file.toString()
+      }, function (res) {
+        console.log(res);
+        (res.error === null).should.be.true;
+        done();
+      });
+    });
+  });
+
+  it('Should process invalid Jade and give back an error', function (done) {
+    fs.readFile(__dirname + '/broken.jade', function (error, file) {
+      requester.send({
+        language: 'jade',
+        source: file.toString()
+      }, function (res) {
+        console.log(res);
+        res.error.should.exist;
+        done();
+      });
+    });
+  });
+
+  after(function () {
+    requester.close();
+  });
+
+});
+
+
