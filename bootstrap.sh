8c95fc9ea3259f5a746691b272c5a11b114309e8
Giulia Alfonsi <electric.g@gmail.com>
2014-07-10 09:52:17 +0100

    Added a comment for an alternative way to install the gems

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,14 @@
 # sudo apt-get install -y ruby
 # sudo apt-get install -y ruby1.9.3-dev
 # sudo apt-get install -y make
+
 gem install bundler
 bundle install
+
+# gem update --system
+# gem install --no-user-install --no-document --pre compass
+# gem install --no-user-install --no-document bourbon
+
 cd vendor/sass-frameworks
 bourbon install
 cd ../../targets/sass/output
