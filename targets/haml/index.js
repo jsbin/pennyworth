7e278ae277b69ab80557da19a04fd8dcf1617dac
Giulia Alfonsi <electric.g@gmail.com>
2014-07-21 18:36:47 +0100

    Added error handling and code for test

--- targets/haml/index.js
+++ targets/haml/index.js
@@ -9,6 +9,8 @@ var spawn = require('child_process').spawn;
 module.exports = function (resolve, reject, data) {
   var par = data.par || module.exports.par;
 
+  // --suppress-eval is the option to disable attribute hashes and Ruby scripts
+  // designated by = or ~ - said scripts are rendered as empty strings
   var args = ['--stdin', '--suppress-eval'];
 
   if (par) {
@@ -32,19 +34,39 @@ module.exports = function (resolve, reject, data) {
     error += data;
   });
 
-  console.log('result', result);
-  console.log('error', error);
-
   haml.on('error', function (error) {
     reject(error);
   });
 
   haml.on('close', function () {
-    if (error) {
-      return reject(error);
+    if (error.indexOf('error on line') !== -1) {
+      var errors = [];
+      var resultArr = error.split('\n');
+      resultArr.forEach(function (line) {
+        // index starts at 1
+        line.trim().replace(/on line\s+([\d]+):\s*(.*?)$/g, function (a, n, e) {
+          var l = parseInt(n, 10) || 0;
+          if (l > 0) {
+            l = l - 1;
+          }
+          errors.push({
+            line: l,
+            ch: null,
+            msg: e
+          });
+        });
+      });
+      // send the errors so we can show them
+      return resolve({
+        errors: errors,
+        result: null
+      });
     }
 
-    resolve({ "result": result});
+    resolve({
+      errors: null,
+      result: result
+    });
   });
 
   // write to stdin and then close the stream to get the result
